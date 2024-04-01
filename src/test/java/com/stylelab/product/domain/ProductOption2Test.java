66579098bed0a939ec8e5e0509e1bfdc1c98cf69
rbsks <rbsks147@daum.net>
2024-04-01 20:40:18 +0900

    productOption2 domain test code

--- src/test/java/com/stylelab/product/domain/ProductOption2Test.java
+++ src/test/java/com/stylelab/product/domain/ProductOption2Test.java
@@ -1,18 +1,26 @@
 package com.stylelab.product.domain;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductOption2Test {
 
     @Test
-    @DisplayName("")
-    public void test() throws Exception {
+    public void initProductOption1Id() throws Exception {
         //given
+        ProductOption2 productOption2 = ProductOption2.builder()
+                .productOption2Id(1L)
+                .option2Name("M")
+                .quantity(100)
+                .build();
 
         //when
+        Long productOption1Id = 2L;
+        ProductOption2 initOption1IdProductOption2 = productOption2.initProductOption1Id(productOption1Id);
 
         //then
-
+        assertThat(initOption1IdProductOption2).isNotEqualTo(productOption2);
+        assertThat(initOption1IdProductOption2.productOption1Id()).isEqualTo(productOption1Id);
     }
 }
